54b1b92bad0bdb2a9bb34c5acebab985a09a31c5
Syed Muhammad Taimour <taimurayaz281@gmail.com>
2026-03-01 18:11:35 +0000

    datatypes done

---
+++ basics1/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as a new version 
+
+// alert(3+3); we are using node js not browser
+
+// code must be readable
+
+let name = "taimur" // string datatype
+let age = 23 // numberd datatype
+let isLogedin = true // bolean datatypes
+
+//DATATYPES
+
+//01 premative datatypes
+
+// number => 2 to 52 or else, or bigint
+// string => ""     
+// boolean => true or false
+// null => standalone value
+// undefine => value not yet assigned
+// symbol => uniqueness 
+
+
+// object
+
+console.log(typeof "taimur"); // check the datatype
+console.log(typeof null)
