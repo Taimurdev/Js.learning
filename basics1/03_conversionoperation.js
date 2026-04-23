012713d04b2e2f5283d4e3b06506c6cd19b88d0e
Syed Muhammad Taimour <taimurayaz281@gmail.com>
2026-04-23 20:51:44 +0000

    conversion of datatype done

---
+++ basics1/03_conversionoperation.js
@@ -0,0 +1,26 @@
+let score = 33
+
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// 33 => 33
+// 33abc => NaN Not a number
+// true => 1 , False => 0
+
+let isLogedin = 1
+
+let booleanIsLogedIn = Boolean(isLogedin)
+
+// console.log(booleanIsLogedIn);
+
+// 1 => true, 0 => false
+// "" => false
+// "Taimur" => true`
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+
+console.log(typeof stringNumber);
+
