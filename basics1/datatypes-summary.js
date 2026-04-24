f226cd64a455e38f5e423e260488273e0d22615a
Syed Muhammad Taimour <taimurayaz281@gmail.com>
2026-04-24 21:58:38 +0000

    datatypes summary done

---
+++ basics1/datatypes-summary.js
@@ -0,0 +1,21 @@
+// primitive 
+
+// 7 types; string, number, boolean, null, undefined, bigInt, symbol
+
+// Reference (Non-primitive) datatypes
+
+// array, functions, objects
+
+const heros = ["aimal", "asimmunir", "shahbaz"]
+
+let myObj = {
+    name: taimur,
+    age: 22,
+
+}
+
+const myFunction = function(){
+
+    console.log("hello world");
+    
+}
