b523dd08fb9cbb2212d0e3f5436313b0fe09f086
Syed Muhammad Taimour <taimurayaz281@gmail.com>
2026-04-24 22:25:10 +0000

    memory done

--- basics1/datatypes-summary.js
+++ basics1/datatypes-summary.js
@@ -6,16 +6,43 @@
 
 // array, functions, objects
 
-const heros = ["aimal", "asimmunir", "shahbaz"]
+// const heros = ["aimal", "asimmunir", "shahbaz"]
 
-let myObj = {
-    name: taimur,
-    age: 22,
+// let myObj = {
+//     name: taimur,
+//     age: 22,
 
-}
+// }
 
-const myFunction = function(){
+// const myFunction = function(){
 
-    console.log("hello world");
+    // console.log("hello world");
     
+// }
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// memories in javascript
+
+//  stack (primitives), Heap(Non-primitives)
+
+// let myNickname = "taimi"
+
+// let anothername = myNickname
+// anothername = "taimurshah"
+
+// console.log(myNickname);
+// console.log(anothername);
+
+let user1 = {
+    email: "taimur@123",
+    upi: "tat@12"
+
 }
+
+let user2 = user1
+
+user2.email = "hi@123"
+ 
+console.log(user1.email);
+console.log(user2.email);
