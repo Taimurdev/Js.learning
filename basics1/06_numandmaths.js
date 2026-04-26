18afda48c6ee36b30b0f51ad4c985e00e484032c
Syed Muhammad Taimour <taimurayaz281@gmail.com>
2026-04-26 09:34:55 +0000

    Numbers and Maths done

---
+++ basics1/06_numandmaths.js
@@ -0,0 +1,35 @@
+const score = 100
+// console.log(score);
+
+const balance = new Number(1000)
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toLocaleString());
+// console.log(balance.toExponential());
+
+const anotherNumber = 100.345
+// console.log(anotherNumber.toPrecision(4));
+// console.log(anotherNumber.toFixed());
+
+// +++++++++++++++ Maths ++++++++++++++
+
+// console.log(Math.abs(-5));
+// console.log(Math.floor(23.9));
+// console.log(Math.ceil(23.9));
+// console.log(Math.min(2, 3, 9));
+// console.log(Math.max(2, 3, 9));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 20
+const max = 40
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
