9641e08e77c532eb5f742c054040e6dc26e53e31
Syed Muhammad Taimour <taimurayaz281@gmail.com>
2026-05-06 20:05:21 +0000

    objects1 done

---
+++ basics2/03_objects.js
@@ -0,0 +1,41 @@
+// singleton
+//
+
+// const mySym = Symbol("key1")
+
+const JsUser = {
+    name: "taimur",
+    // "full name": "Taimur shah"
+//    [ mySym]: "mykey1"
+    age: 28,
+    email: "taimur@13.com",
+    location: "pakistan",
+    isLogedIn: false,
+    loggedInDays: ["monday", "Friday"]
+}
+
+// console.log(JsUser.email);
+// console.log(JsUser["email"]);
+
+// JsUser.email = "taimur.com"
+// Object.freeze(JsUser)
+// JsUser.email = "taimur.npcom"
+// console.log(JsUser);
+
+JsUser.greeting = function() {
+    console.log("Hello JS User");
+    
+}
+JsUser.greeting2 = function() {
+    console.log(`hello JS user, ${this.name}`);
+    
+}
+console.log(JsUser.greeting());
+console.log(JsUser.greeting2());
+
+
+
+
+
+
+
