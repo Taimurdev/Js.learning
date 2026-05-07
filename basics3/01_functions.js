2f544e558224e1559403fb5de5832402f7493e0f
Syed Muhammad Taimour <taimurayaz281@gmail.com>
2026-05-07 13:18:37 +0000

    functions done

---
+++ basics3/01_functions.js
@@ -0,0 +1,29 @@
+function sayMyName(){
+    console.log("T");
+    console.log("a");
+    console.log("i");
+    console.log("m");
+    console.log("u");
+    console.log("r");
+    
+// }
+// sayMyName()
+
+
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2);
+     
+}
+function addTwoNumbers(number1, number2){
+    let result = number1 + number2
+    return result
+ 
+}
+
+const result = addTwoNumbers(4, 7)
+ addTwoNumbers(4, 7)
+
+console.log("Result: ", result);
+
+
+
