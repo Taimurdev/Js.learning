81903df9317298ab93f4025ad5c83528b9d6f2bd
Syed Muhammad Taimour <taimurayaz281@gmail.com>
2026-05-14 19:55:51 +0000

    controlflow one

---
+++ 04_control_flow/one.js
@@ -0,0 +1,36 @@
+// const balance = 1000
+
+// if(balance > 500){
+//     console.log("test");
+    
+// }
+
+// if (balance < 500){
+    
+//     console.log("less than 500");
+    
+
+// } else if(balance < 750){
+//     console.log("less than 750");
+
+    
+// } else if (balance < 900){
+//     console.log("less than 900");
+    
+// }else {
+//     console.log("less than 1200");
+    
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+ 
+if(userLoggedIn && debitCard && 2==3){
+    console.log("user is allow to buy courses");
+}
+if (loggedInFromGoogle || loggedInFromEmail){
+    console.log("user logged in");
+    
+}
